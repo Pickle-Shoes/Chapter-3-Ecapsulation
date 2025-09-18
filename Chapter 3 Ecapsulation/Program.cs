7cd3be8219653cf5a21789b4879ed9b58f607c4c
Luke Martin <luke.martin@trojans.dsu.edu>
2025-09-18 14:50:33 -0500

    Add project files.

---
+++ Chapter 3 Ecapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter_3_Ecapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Chapter 3 Ecapsulation/SmoothFoxTerrier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3_Ecapsulation
+{
+    internal class SmoothFoxTerrier
+    {
+        //Class fields/Attributes
+        public static string Family;
+        public static string AreaOfOrgin;
+        public static int Energy;
+        public static int ColdTolerance;
+        public static int HeatTolerance;
+        
+        //instance feild/Attributes
+        public string Name;
+        public int WatchdogAbility;
+        public string FavoriteToy;
+
+
+        //Contructor
+        public SmoothFoxTerrier(string name, int w, string favToy)
+        {
+            this.Name = name;
+            this.WatchdogAbility = w;
+            this.FavoriteToy = favToy;
+        }
+    }
+}
