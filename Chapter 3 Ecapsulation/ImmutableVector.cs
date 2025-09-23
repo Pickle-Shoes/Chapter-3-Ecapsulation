b6fb4050ad6b1039ce5f1d29cb2a5a9bc8e95a4b
Luke Martin <luke.martin@trojans.dsu.edu>
2025-09-23 15:18:05 -0500

    ImmutableVector created

---
+++ Chapter 3 Ecapsulation/ImmutableVector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3_Ecapsulation
+{
+    internal class ImmutableVector
+    {
+        //read-only properties
+        public double X { get; private set;}
+        public double Y { get; private set;}
+        public double Z { get; private set;}
+
+
+        //Constructor
+        public ImmutableVector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        //Method
+        public ImmutableVector sum(double deltaX, double deltaY, double deltaZ)
+        {
+            //dont overwrite values, instead create new vector with new values
+            return new ImmutableVector(X + deltaX, Y + deltaY, Z + deltaZ);
+        }
+
+    }
+}
